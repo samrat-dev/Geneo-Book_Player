dda8e2cb61d9bae293235660066ba6181e0c9e70
Samrat <samratdot@outlook.com>
2021-01-10 13:57:41 +0530

    changes in practise js

--- src/containers/Practise/Practise.js
+++ src/containers/Practise/Practise.js
@@ -6,7 +6,7 @@ const WelcomePractise = <div style={{ padding: '1rem' }}>
     Practise <br />
     Tutorial Continue at :
     <a target='tutorial'
-        href='https://www.youtube.com/watch?v=qriWTj7xlbg&list=PLZjjdd9-SJS0UiiJ0-dYk_PpI5GjYQ-Eb&index=2'>This Link</a> <br />
+        >This Link</a> <br />
 </div>
 
 export default function Practise(props) {
