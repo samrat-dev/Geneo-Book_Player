7a2ca46edeab900a683c078a731140a6a621babf
Samrat <samratdot@outlook.com>
2021-01-18 03:42:27 +0530

    add shop page inner components

--- src/containers/Practise/pages/shop/shop.js
+++ src/containers/Practise/pages/shop/shop.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import SHOP_DATA from './shop.data';
+import SHOP_DATA from "./shop.data";
 
+import PreviewCollection from "../../components/preview-collection/preview-collection";
 class ShopPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        collections: SHOP_DATA
-    }
+      collections: SHOP_DATA,
+    };
   }
   render() {
-    return <div>SHOP_DATA PAGE</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...others }) => (
+          <PreviewCollection key={id} {...others} />
+        ))}
+      </div>
+    );
   }
 }
 
