695b489eca6913e7bf68a7937f26fa4b369c09b9
Samrat <samratdot@outlook.com>
2021-05-09 23:11:38 +0530

    .

--- src/containers/Practise/components/cart-dropdown/cart-dropdown.js
+++ src/containers/Practise/components/cart-dropdown/cart-dropdown.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button';
+import CartItem from '../cart-item/cart-item';
+
 import './cart-dropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
-        <div className="cart-items" />
+        <div className="cart-items">
+            {
+                cartItems.map(item => <CartItem key={item.id} item={item} />)
+            }
+        </div>
         <CustomButton>Go to Checkout</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+const mapstateToProps = state => ({
+    cartItems: state.cart.cartItems
+});
 
+export default connect(mapstateToProps)(CartDropdown);
--- src/containers/Practise/components/cart-icon/cart-icon.js
+++ src/containers/Practise/components/cart-icon/cart-icon.js
@@ -5,16 +5,21 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as Icon } from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
 
-const cartIcon = ({ toggleCart }) => (
+const cartIcon = ({ toggleCart, itemCount }) => (
     <div className="cart-icon" onClick={toggleCart}>
         <Icon className="shooping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
+const mapstateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((total, item) => {
+        return total + item.quantity
+    }, 0)
+});
 const mapDispatchToProps = dispatch => ({
     toggleCart: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(cartIcon);
+export default connect(mapstateToProps, mapDispatchToProps)(cartIcon);
 
