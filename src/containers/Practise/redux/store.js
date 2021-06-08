d9a7b952c4aca961b547e5c3dff77022ac4dd746
Samrat <samratdot@outlook.com>
2021-06-09 00:11:01 +0530

    .

--- src/containers/Practise/redux/store.js
+++ src/containers/Practise/redux/store.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
 import logger from 'redux-logger';
 
-const middlewares = [logger];
+const middlewares = [];
+// const middlewares = [logger];
 
 const store = createStore(
     rootReducer,
--- src/containers/ReduxPractise/ReduxPractise.js
+++ src/containers/ReduxPractise/ReduxPractise.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+// Lazy Initialization
+const lazyValue = () => {
+    return 'Hello';
+}
 export default function ReduxPractise() {
-    // https://www.youtube.com/watch?v=6Q2B7Dv0OiQ
-    
+    const [a, setA] = useState(lazyValue);
     return (
         <div className="ReduxPractise">
             <h1>
                 Redux Practise
-          </h1>
+            </h1>
+            <h3>
+                {a}
+            </h3>
         </div>
     )
 }
+
+
+
+
+    // https://www.youtube.com/watch?v=324czHa9O9U&list=PL0CUCu7IoPKj67UX4dTWbxzaykqMgErDG&index=5&t=3643s
+    // https://www.youtube.com/watch?v=6Q2B7Dv0OiQ
